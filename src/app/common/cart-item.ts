b74efacdcc70ed4c0c4bfa06810bdf9a2a5d5c47
Emil Romanov <eromanov8@gmail.com>
2023-10-09 16:43:40 +0300

    cart service generation

---
+++ src/app/common/cart-item.ts
@@ -0,0 +1,10 @@
+import { Product } from "./product";
+
+export class CartItem {
+
+    constructor(public id: string,
+                public name: string,
+                public imageUrl: string,
+                public unitPrice: number,
+                public quantity: number = 1) {}
+}
