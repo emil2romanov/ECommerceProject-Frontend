ee91eee939266c7fe48267497c6ef7fdd715463d
Emil Romanov <eromanov8@gmail.com>
2023-10-09 14:04:14 +0300

    search products by keyword

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -50,6 +50,17 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
+  searchProductsPaginate(thePage: number, 
+                         thePageSize: number,
+                         theKeyword: string): Observable<GetResponseProducts> {
+
+    // need to build URL based on keyword, page and size
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+}
+
   private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
