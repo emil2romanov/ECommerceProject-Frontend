dc90049271b9737e0406804580225f97fe0c58f5
Emil Romanov <eromanov8@gmail.com>
2023-10-09 13:55:24 +0300

    selecting page size

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -17,7 +17,7 @@ export class ProductListComponent implements OnInit {
 
   // new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
   
   constructor(private productService: ProductService,
@@ -91,5 +91,11 @@ export class ProductListComponent implements OnInit {
                                                   this.theTotalElements = data.page.totalElements;
                                                 }
                                                );
+  }
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 }
