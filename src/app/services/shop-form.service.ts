0545eec5a9c433cd1bb61f96319a1f1c6f1fbbce
Emil Romanov <eromanov8@gmail.com>
2023-10-10 16:34:09 +0300

    countries and states

--- src/app/services/shop-form.service.ts
+++ src/app/services/shop-form.service.ts
@@ -1,12 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Country } from '../common/country';
+import { map } from 'rxjs/operators';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopFormService {
 
-  constructor() { }
+  private countriesUrl = "http://localhost:8080/api/countries";
+  private statesUrl = "http://localhost:8080/api/states";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCountries(): Observable<Country[]> {
+
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]> {
+
+    // search url
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
 
@@ -15,7 +39,7 @@ export class ShopFormService {
     // build an array for "Month" dropdown list
     // - start at current month and loop until
 
-    for(let theMonth = startMonth; theMonth <= 12; theMonth++) {
+    for (let theMonth = startMonth; theMonth <= 12; theMonth++) {
       data.push(theMonth);
     }
 
@@ -32,11 +56,23 @@ export class ShopFormService {
     const startYear: number = new Date().getFullYear();
     const endYear: number = startYear + 10;
 
-    for(let theYear = startYear; theYear <= endYear; theYear++) {
+    for (let theYear = startYear; theYear <= endYear; theYear++) {
       data.push(theYear);
     }
 
     return of(data);
   }
 
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
+  }
 }
