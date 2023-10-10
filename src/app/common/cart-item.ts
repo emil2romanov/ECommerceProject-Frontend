c981a43d7ad6f367ded69780471f99646063f7e2
Emil Romanov <eromanov8@gmail.com>
2023-10-10 09:53:50 +0300

    fixing other bugs

--- src/app/common/cart-item.ts
+++ src/app/common/cart-item.ts
@@ -2,9 +2,20 @@ import { Product } from "./product";
 
 export class CartItem {
 
-    constructor(public id: string,
-                public name: string,
-                public imageUrl: string,
-                public unitPrice: number,
-                public quantity: number = 1) {}
+    id: string;
+    name: string;
+    imageUrl: string;
+    unitPrice: number;
+
+    quantity: number;
+
+    constructor(product: Product) {
+        this.id = product.id;
+        this.name = product.name;
+        this.imageUrl = product.imageUrl;
+        this.unitPrice = product.unitPrice;
+
+        this.quantity = 1;
+
+    }
 }
--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,15 +1,13 @@
 export class Product {
 
-    constructor(public id: number,
-        public sku: string,
-        public name: string,
-        public description: string,
-        public unitPrice: number,
-        public imageUrl: string,
-        public active: boolean,
-        public unitsInStock: number,
-        public dateCreated: Date,
-        public lastUpdated: Date) {
-
-    }
+    id: string;
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdated: Date;
 }
