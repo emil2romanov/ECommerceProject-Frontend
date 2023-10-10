5375f7e50902681fdf6ed9e3b01f643e3ded08b2
Emil Romanov <eromanov8@gmail.com>
2023-10-10 14:57:41 +0300

    fixing months and years drop-down list

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -95,4 +95,29 @@ export class CheckoutComponent implements OnInit {
     }
   }
 
+  handleMonthsAndYears() {
+    
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+
+    let startMonth: number;
+
+    if(currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    } else {
+      startMonth = 1;
+    }
+
+    this.shopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    )
+  }
+
 }
