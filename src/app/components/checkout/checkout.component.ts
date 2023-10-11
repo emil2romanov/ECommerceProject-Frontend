af20d8519d3589fc77076876910917b01d57a387
Emil Romanov <eromanov8@gmail.com>
2023-10-11 13:33:20 +0300

    getStates feature

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -92,6 +92,9 @@ export class CheckoutComponent implements OnInit {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer').value);
     console.log("The email adress is " + this.checkoutFormGroup.get('customer').value.email);
+
+    console.log("The shipping adress country is " + this.checkoutFormGroup.get('shippingAddress').value.country.name);
+    console.log("The shipping adress country is " + this.checkoutFormGroup.get('shippingAddress').value.state.name);
   }
 
   copyShippingAddressToBillingAddress(event) {
@@ -142,8 +145,8 @@ export class CheckoutComponent implements OnInit {
     const countryCode = formGroup.value.country.code;
     const countryName = formGroup.value.country.name;
 
-    console.log(`{formGroupName} country code: ${countryCode}`);
-    console.log(`{formGroupName} country name: ${countryName}`);
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
 
     this.shopFormService.getStates(countryCode).subscribe(
       data => {
