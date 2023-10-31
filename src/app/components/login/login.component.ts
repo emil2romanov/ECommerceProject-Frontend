a1c7f1ebb2ce377395e342fa4a1cc4fb70c50d56
Emil Romanov <eromanov8@gmail.com>
2023-10-31 16:42:45 +0200

    login components

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,10 +15,33 @@ export class LoginComponent implements OnInit {
 
   constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {
 
-    this.oktaSignin = new OktaSignIn();
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
    }
 
   ngOnInit(): void {
+    this.oktaSignin.remove();
+
+    this.oktaSignin.renderEl({
+      el: '#okta-sign-in-widget'}, // this name should be the same as div tag id in login.component.html
+      (response: any) => {
+        if(response.status === 'SUCCESS') {
+          this.oktaAuth.signInWithRedirect();
+        }
+      },
+      (error: any) => {
+        throw error;
+      }
+    );
   }
 
 }
